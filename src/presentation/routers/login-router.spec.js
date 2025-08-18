153094cc1edcae3ed0fc2ad34b52562de6d6bd4c
Cicero Alvarenga <cicero.neto@sysmap.com.br>
2025-08-19 04:19:42 +0800

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -61,6 +61,7 @@ describe('Login Router', () => {
 
   test('Should call AuthUseCase with correct params', () => {
     const { sut, authUseCaseSpy } = makeSut();
+
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -86,5 +87,35 @@ describe('Login Router', () => {
 
     expect(httpResponse.statusCode).toBe(401);
     expect(httpResponse.body).toEqual(new UnauthorizedError());
+  });
+  test('Should return 500 if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+  });
+  test('Should return 500 if AuthUseCase has no auth method', () => {
+    class AuthUseCaseSpy {}
+
+    const authUseCaseSpy = new AuthUseCaseSpy();
+
+    const sut = new LoginRouter(authUseCaseSpy);
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
